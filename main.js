e18038bc6c096fb616bce55c50013aadbfd9a976
KnfeConcorde <80136205+KnfeConcorde@users.noreply.github.com>
2024-02-12 12:41:14 +0800

    a lot of shit

--- main.js
+++ main.js
@@ -1,34 +1,40 @@
-function filterTestimonials() {
-  // Get the search input value and split it into an array of keywords
-  var searchInput = document.getElementById("search-input").value.toLowerCase().split(" ");
-  console.log("Search input:", searchInput);
+function filterTestimonials(event) {
+  // Check if the input event's data property is a space character
+  if (event.data === " ") {
+    // If it is, prevent the default behavior of adding a space character to the search input
+    event.preventDefault();
+  } else {
+    // Get the search input value and split it into an array of keywords
+    var searchInput = document.getElementById("search-input").value.toLowerCase().split(" ");
+    console.log("Search input:", searchInput);
 
-  // Get all the testimonial-filter elements
-  var testimonialFilters = document.getElementsByClassName("testimonial-filter");
-  console.log("Number of testimonial filters:", testimonialFilters.length);
+    // Get all the testimonial-filter elements
+    var testimonialFilters = document.getElementsByClassName("testimonial-filter");
+    console.log("Number of testimonial filters:", testimonialFilters.length);
 
-  // Loop through the testimonial-filter elements and filter based on user input
-  for (var i = 0; i < testimonialFilters.length; i++) {
-    var testimonialFilter = testimonialFilters[i];
+    // Loop through the testimonial-filter elements and filter based on user input
+    for (var i = 0; i < testimonialFilters.length; i++) {
+      var testimonialFilter = testimonialFilters[i];
 
-    // Get the text content of the testimonial-filter element and split it into words
-    var words = (testimonialFilter.textContent || testimonialFilter.innerText).toLowerCase().split(" ");
-    console.log("Number of words in testimonial filter:", words.length);
+      // Get the text content of the testimonial-filter element and split it into words
+      var words = (testimonialFilter.textContent || testimonialFilter.innerText).toLowerCase().split(" ");
+      console.log("Number of words in testimonial filter:", words.length);
 
-    // Check if any of the keywords in the search input match any of the words in the testimonial-filter element
-    var matchFound = searchInput.some(function(keyword) {
-      return words.some(function(word) {
-        return word.includes(keyword);
+      // Check if any of the keywords in the search input match any of the words in the testimonial-filter element
+      var matchFound = searchInput.some(function(keyword) {
+        return words.some(function(word) {
+          return word.includes(keyword);
+        });
       });
-    });
-    console.log("Match found in testimonial filter:", matchFound);
+      console.log("Match found in testimonial filter:", matchFound);
 
-    // If a match was found, show the testimonial-filter element
-    if (matchFound) {
-      testimonialFilter.style.display = "flex";
-    } else {
-      // If no match was found, hide the testimonial-filter element
-      testimonialFilter.style.display = "none";
+      // If a match was found, show the testimonial-filter element
+      if (matchFound) {
+        testimonialFilter.style.display = "flex";
+      } else {
+        // If no match was found, hide the testimonial-filter element
+        testimonialFilter.style.display = "none";
+      }
     }
   }
 }
--- nav.js
+++ nav.js
@@ -1,19 +1,24 @@
 function showMenu() {
-    document.getElementById("nav-links").style.right = "0";
-    document.querySelector("nav .fa").style.display = "none";
-    document.querySelector("nav .fa-times").style.display = "block";
-    document.querySelector("nav .fa-times").style.animation = "rotate 0.5s linear";
-  }
-  
-  function hideMenu() {
-    document.getElementById("nav-links").style.right = "-200px";
-    document.querySelector("nav .fa").style.display = "block";
-    document.querySelector("nav .fa-times").style.display = "none";
-  }
-  
-  function toggleMenu() {
-    document.getElementById("nav-links").classList.toggle("open");
-    document.querySelector("nav .fa").style.display = "none";
-    document.querySelector("nav .fa-times").style.display = "block";
-    document.querySelector("nav .fa-times").style.animation = "rotate 0.5s linear";
-  }
+  document.getElementById("nav-links").style.right = "0";
+  document.querySelector("nav .fa").style.display = "none";
+  document.querySelector("nav .fa-times").style.display = "block";
+  document.querySelector("nav .fa-times").style.animation = "rotate 0.5s linear";
+  document.body.classList.add("no-scroll");
+}
+
+function hideMenu() {
+  document.getElementById("nav-links").style.right = "-200px";
+  document.querySelector("nav .fa").style.display = "block";
+  document.querySelector("nav .fa-times").style.display = "none";
+  document.querySelector("nav .fa-times").style.animation = "";
+  document.body.classList.remove("no-scroll");
+  document.body.classList.add("no-scroll");
+}
+
+function toggleMenu() {
+  document.getElementById("nav-links").classList.toggle("open");
+  document.querySelector("nav .fa").style.display = "none";
+  document.querySelector("nav .fa-times").style.display = "block";
+  document.querySelector("nav .fa-times").style.animation = "rotate 0.5s linear";
+  document.body.classList.toggle("no-scroll");
+}
